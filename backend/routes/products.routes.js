7369075d2879955b2105d8e60c5cea4b3d2a1607
Chris Wander <chriswandermail@gmail.com>
2020-08-31 14:02:26 -0400

    modifying product table
    product table

--- backend/routes/products.routes.js
+++ backend/routes/products.routes.js
@@ -9,19 +9,24 @@ const { productController } = require('../controllers');
 //
 
 router.get('/', productController.findAllProducts); // all products
-router.get('/sku/:id', productController.findProduct); // find one product by id
+
+
 router.get('/rent', productController.findAllProductsForRent); // all products for rent
+
 router.get(
 	'/available-rent/:start/:end',
 	productController.findAllProductsForRentAndAvailable
 ); // all products for rent
 
+
 router.get('/sale', productController.findAllProductsForSale); // all products for sale
 router.get('/out', productController.findAllProductsOutOfStock); // all products out of stock
-
 router.get('/rentals', productController.findAllRentals);
 
-router.post('/', productController.createProductDB); // create product
+
+
+router.post('/', productController.createProductDB); // create product on DB
+
 
 router.put('/update/:id', productController.updateProduct);
 
--- frontend/src/pages/Admin/AllProducts.jsx
+++ frontend/src/pages/Admin/AllProducts.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import { makeStyles,  } from '@material-ui/core/styles';
-
+import { makeStyles  } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
@@ -10,30 +9,30 @@ import AllProductsTable from '../../components/Tables/AllProductsTable'
 import ProductsChanges from './ProductsChanges';
 
 
-const useStyles2 = makeStyles((theme) => ({
-	root: {
-		flexShrink: 3,
-	},
+const useStyles = makeStyles((theme) => ({
+	// root: {
+	// 	flexShrink: 3,
+	// },
 	table: {
 		minWidth: 400,
 	},
 }));
 
 const AllProducts = React.memo(() => {
-	const classes = useStyles2();
+	const classes = useStyles();
 
 
 
 	return (
-		<Grid container spacing={2} justify="center" className={classes.root}>
-			<Grid item xs={12}>
+		<Grid container justify="center" className={classes.root}>
+			<Grid item xs={6}>
 				<Typography variant="h3">Products</Typography>
 			</Grid>
-			<Grid item xs={12} md={10}>
+			<Grid item xs={11} sm={10}>
 			<AllProductsTable/>
 			</Grid>
 
-			<Grid item xs={12}>
+			<Grid item xs={10}>
 				<ProductsChanges />
 			</Grid>
 		</Grid>
