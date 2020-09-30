de3e440b11d765c2c16a9757a31378fd2edc6144
Chris Wander <chriswandermail@gmail.com>
2020-09-30 15:47:13 -0400

    mixed routes
    routes, sales controller

--- backend/routes/products.routes.js
+++ backend/routes/products.routes.js
@@ -2,28 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { productController } = require('../controllers');
 
-// routes
+// all products * no queries
+router.get('/', productController.findAllProducts);
 
-//
-// GET
-//
-
-router.get('/', productController.findAllProducts); // all products
-
-router.get('/rent', productController.findAllProductsForRent); // all products for rent
-
-router.get(
-	'/available-rent/:start/:end',
-	productController.findAllProductsForRentAndAvailable
-); // all products for rent
-
-router.get('/sale', productController.findAllProductsForSale); // all products for sale
-router.get('/sales', productController.findAllSales); // all products for sale
-router.get('/recent-sales', productController.findRecentSales); // all products for sale
 router.get('/out', productController.findAllProductsOutOfStock); // all products out of stock
-
-router.get('/rentals', productController.findAllRentals);
-router.get('/active-rentals', productController.findAllActiveRentals);
 
 router.post('/', productController.createProductDB); // create product on DB
 
--- backend/routes/sales.routes.js
+++ backend/routes/sales.routes.js
@@ -4,8 +4,11 @@ const { salesController } = require('../controllers');
 
 router.get('/', salesController.findAllProductsForSale);
 router.get('/:id', salesController.findProductsById);
-router.get('/:category', salesController.findAllProductsByCategory);
-router.get('/:sub-category', salesController.findAllProductsBySubCategory);
-
-
+router.get('/category/:category', salesController.findAllProductsByCategory);
+router.get(
+	'/sub-category/:subcategory',
+	salesController.findAllProductsBySubCategory
+);
+router.get('/admin/all-sales', salesController.findAllSales); // all products for sale
+router.get('/admin/recent-sales', salesController.findRecentSales); // all products for sale
 module.exports = router;
