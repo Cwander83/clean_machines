33a87427dbf522f293d604e7082a8bef3e260598
Chris Wander <chriswandermail@gmail.com>
2020-12-10 20:12:11 -0500

    deploy

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,13 +1,21 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-	process.env.DB_DATABASE,
-	process.env.DB_USER,
-	process.env.DB_PASSWORD,
+	// process.env.DB_DATABASE,
+	// process.env.DB_USER,
+	// process.env.DB_PASSWORD,
+	// {
+	// 	host: '127.0.0.1',
+	// 	dialect: 'mysql',
+	// }
+	process.env.NODECHEF_DATABASE_NAME,
+	process.env.NODECHEF_USER,
+	process.env.NODECHEF_PASSWORD,
 	{
-		host: '127.0.0.1',
+		host: process.env.NODECHEF_HOST,
 		dialect: 'mysql',
 	}
+	
 );
 
 const db = {};
